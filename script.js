3640125659d936490ee0c4f94ef8f94423b948e9
jaswanthchowdary99 <154533631+jaswanthchowdary99@users.noreply.github.com>
2024-10-19 16:10:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const byteSize = (str) => {
   // write your code here
+	let size = 0;
+		for(let i = 0; i < str.length; i++){
+			let charCode = str.charCodeAt(i);
+
+			if(charCode <= 0x7F){
+				size += 1;
+			}else if(charCode <= 0x7FF){
+				size += 2;
+			}else{
+				size += 3;
+			}
+		}
+	return size;
 };
 
 // Do not change the code below
